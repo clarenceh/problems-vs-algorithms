473054327b1ded40d14b8f90e75a2fd3223e4fac
clarence <clarence@you.co>
2019-06-04 17:25:02 +0800

    Added problem 5 files

---
+++ problem_5.py
@@ -0,0 +1,38 @@
+## Represents a single node in the Trie
+class TrieNode:
+    def __init__(self):
+        ## Initialize this node in the Trie
+
+        # A dictionary to store the possible characters in this node
+        # Dictionary key: character (e.g. a)
+        # Dictionary value: a tuple (end_of word: boolean, child_node)
+        self.char_dict = dict()
+
+    def insert(self, char):
+        ## Add a child node in this Trie
+        pass
+
+
+## The Trie itself containing the root node and insert/find functions
+class Trie:
+    def __init__(self):
+        ## Initialize this Trie (add a root node)
+        self.root = None
+
+    def insert(self, word):
+        ## Add a word to the Trie
+        pass
+
+    def find(self, prefix):
+        ## Find the Trie node that represents this prefix
+        pass
+
+
+MyTrie = Trie()
+wordList = [
+    "ant", "anthology", "antagonist", "antonym",
+    "fun", "function", "factory",
+    "trie", "trigger", "trigonometry", "tripod"
+]
+for word in wordList:
+    MyTrie.insert(word)
