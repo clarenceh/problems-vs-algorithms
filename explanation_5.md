83da1eb7a638d4e09d447f31d33a3562b85468cd
clarence <clarence@you.co>
2019-06-05 14:47:28 +0800

    Added problem 5 files

---
+++ explanation_5.md
@@ -0,0 +1,7 @@
+# Problems vs Algorithms - Problem 5 - Autocomplete with Tries - Explanation
+
+## Logic implemented
+TrieNode and Trie classes were implemented.
+
+## Run time complexity
+For lookup, the worst case run time complexity is of O(n).
--- problem_5.py
+++ problem_5.py
@@ -1,31 +1,91 @@
-## Represents a single node in the Trie
+# Represents a single node in the Trie
 class TrieNode:
-    def __init__(self):
-        ## Initialize this node in the Trie
+    def __init__(self, end_of_word=False):
+        # Initialize this node in the Trie
+
+        # Indicates whether the string ends here is a valid word
+        self.end_of_word = end_of_word
 
         # A dictionary to store the possible characters in this node
         # Dictionary key: character (e.g. a)
-        # Dictionary value: a tuple (end_of_word: boolean, child_node)
+        # Dictionary value: pointer to child node
         self.char_dict = dict()
 
     def insert(self, char):
-        ## Add a child node in this Trie
-        pass
+        # Add a child node in this Trie
+        sub_char_node = TrieNode()
+        self.char_dict[char] = sub_char_node
+
+        return sub_char_node
+
+    def suffixes(self, suffix=''):
+        # Recursive function that collects the suffix for
+        # all complete words below this point
+        output_str_list = list()
+
+        def find_suffix(node, output_str):
+
+            # If end_of_word at this node is true, then add the suffix to result list
+            if node.end_of_word:
+                output_str_list.append(output_str)
+
+            for char in node.char_dict:
+                output_str += char
+                find_suffix(node.char_dict[char], output_str)
+
+        find_suffix(self, "")
+
+        return output_str_list
 
 
-## The Trie itself containing the root node and insert/find functions
+# The Trie itself containing the root node and insert/find functions
 class Trie:
     def __init__(self):
-        ## Initialize this Trie (add a root node)
-        self.root = None
+        # Initialize this Trie (add a root node)
+        root_node = TrieNode()
+        self.root = root_node
 
     def insert(self, word):
-        ## Add a word to the Trie
-        pass
+        # Add a word to the Trie
+
+        # Split the word into a seq of chars and build the corresponding TrieNodes
+        cur_node = self.root
+
+        for char in word:
+            if char in cur_node.char_dict:
+                cur_node = cur_node.char_dict[char]
+            else:
+                new_child_node = cur_node.insert(char)
+                cur_node = new_child_node
+
+        # End of word, set end_of_word property to True
+        cur_node.end_of_word = True
 
     def find(self, prefix):
-        ## Find the Trie node that represents this prefix
-        pass
+        # Find the Trie node that represents this prefix
+
+        cur_node = self.root
+        # Traverse the Trie tree base on the character sequence in the prefix
+        for char in prefix:
+            if char in cur_node.char_dict:
+                cur_node = cur_node.char_dict[char]
+            else:
+                return None
+
+        return cur_node
+
+    def __str__(self):
+        output_str = [""]
+
+        def print_node(node, output_str):
+            output_str[0] += f"End of word: {node.end_of_word}\n"
+            for char in node.char_dict:
+                output_str[0] += f"char: {char}\n"
+                print_node(node.char_dict[char], output_str)
+
+        print_node(self.root, output_str)
+
+        return output_str[0]
 
 
 MyTrie = Trie()
@@ -36,3 +96,9 @@ wordList = [
 ]
 for word in wordList:
     MyTrie.insert(word)
+
+print(MyTrie)
+
+node = MyTrie.find('a')
+print(node.suffixes())
+
