0973350789af3a8d89883b6f5ed761d252ca6b8c
clarence <clarence@you.co>
2019-06-10 10:11:47 +0800

    Updated problem 1

--- explanation_1.md
+++ explanation_1.md
@@ -1,7 +1,13 @@
 # Problems vs Algorithms - Problem 1 - Finding the Square Root of an Integer - Explanation
 
 ## Logic implemented
-Binary Search technique was used to locate the square root value.
+Instead of a linear search for the square value, Binary Search algorithm was used to locate the square root value.
+
+The reason is that by adopting Binary Search, in each iteration, the middle value was calculated and it's square value is compared against the target value.
+
+If the result is less than the target value, the result should be less than the middle value, which lies in the "left half" of the middle value. Otherwise, the result should be greater which lies in the "right half" of the middle value.
+
+So in each iteration, half of the values can be eliminated from the search and the efficiency can be greatly increased.
 
 ## Run time complexity
 The run time complexity is the same as a Binary Search, which is of O(log(n))
--- problem_1.py
+++ problem_1.py
@@ -8,6 +8,16 @@ def sqrt(number):
        int: Floored Square Root
     """
 
+    # Handle non-integer inputs
+    try:
+        int(number)
+    except ValueError:
+        return None
+
+    # Handle negative integer
+    if number < 0:
+        return None
+
     # Handle zero value
     if number == 0:
         return 0
@@ -50,3 +60,18 @@ print ("Pass" if  (0 == sqrt(0)) else "Fail")
 print("Calling function with value 27")
 # Should print pass as the square root's floor value is 5
 print ("Pass" if  (5 == sqrt(27)) else "Fail")
+
+# Test case 4 - negative value
+print("Calling function with negative value -10")
+# Should print pass as the function should return None
+print ("Pass" if  (None == sqrt(-10)) else "Fail")
+
+# Test case 5 - float value
+print("Calling function with float value 10.1")
+# Should print pass as the function should return None
+print ("Pass" if  (None == sqrt(10.1)) else "Fail")
+
+# Test case 6 - non numeric
+print("Calling function with non-numeric value abc")
+# Should print pass as the function should return None
+print ("Pass" if  (None == sqrt('abc')) else "Fail")
